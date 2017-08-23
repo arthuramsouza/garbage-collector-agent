1f501de0b097d7931d6618db1224b7c6396d4b73
emanoelvianna <emanoel.vianna@outlook.com>
2017-08-23 02:02:44 -0300

    ambiente - parcial

--- src/Ambiente.java
+++ src/Ambiente.java
@@ -2,7 +2,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Ambiente {
-  private int n;
+  private int tamanho;
+  private int quantidadeLixeiras;
+  private int quantidadeRecargas;
   private char matriz[][];
 
   List<Agente> agentes;
@@ -16,43 +18,73 @@ public class Ambiente {
   public static final char PAREDE = 'P';
   public static final char LIMPO = '.';
 
+  /* Construtor da classe */
+  public Ambiente(int n, int quantidadeLixeiras, int quantidadeRecargas) {
+    agentes = new ArrayList<Agente>();
+    this.tamanho = n;
+    this.quantidadeLixeiras = quantidadeLixeiras;
+    this.quantidadeRecargas = quantidadeRecargas;
+    matriz = new char[tamanho][tamanho];
+    carregar();
+  }
+
   /* TODO Coloca elementos (lixeiras, lixo e carregadores) de forma randomica no ambiente  */
   private void carregar(int n_lixeiras, int n_carregadores, int n_lixo) {
-    for(int i = 0; i < n; i++)
-      for(int j = 0; j < n; j++)
+    for(int i = 0; i < tamanho; i++)
+      for(int j = 0; j < tamanho; j++)
         matriz[i][j] = LIMPO;
   }
 
   /* Coloca elementos no ambiente - estatico */
   private void carregar() {
-    for(int i = 0; i < n; i++)
-      for(int j = 0; j < n; j++)
+    for(int i = 0; i < tamanho; i++)
+      for(int j = 0; j < tamanho; j++)
         matriz[i][j] = LIMPO;
 
     matriz[4][0] = RECARGA;
     matriz[2][0] = LIXEIRA;
     matriz[4][9] = RECARGA;
+
+    inserirParedes();
+    inserirLixeiras();
+    inserirSujeiras();
   }
-
-
-  /* Construtor da classe */
-  public Ambiente(int n) {
-    agentes = new ArrayList<Agente>();
-    this.n = n;
-    matriz = new char[n][n];
-    carregar();
-  }
-
 
   /* Insere agente no ambiente */
   public void inserirAgente(Agente agente) {
     agentes.add(agente);
   }
 
+  /* Insere paredes no ambiente */
+  public void inserirParedes() {
+    // dividindo a matriz em 3 partes
+    int metade = this.tamanho/3;
+    
+    for(int i = 0; i < tamanho; i++) {
+      for(int j = 0; j < tamanho; j++) {
+        // desconsiderando as primeiras linhas da matriz
+        if(i < (this.tamanho-2) || i > 2) {
+          this.matriz[i][metade*2] = PAREDE;
+          this.matriz[i][metade] = PAREDE;
+        }
+      }
+    }
+  }
+
+  //TODO: adicionar lixeiras randomicas considerando as paredes
+  public void inserirLixeiras() {
+
+  }
+
+  //TODO: adicionar sujeiras randomicas considerando as paredes
+  public void inserirSujeiras() {
+
+  }
+
   /* Imprime o estado atual do ambiente com seus agentes */
   public void print() {
-    for(int i = 0; i < n; i++) {
-      for(int j = 0; j < n; j++) {
+    for(int i = 0; i < tamanho; i++) {
+      for(int j = 0; j < tamanho; j++) {
 
         // Verifica se algum agente esta nesta Posicao
         for(Agente ag : agentes) {
@@ -86,7 +118,7 @@ public class Ambiente {
           else
             esq = matriz[ag.getY()][ag.getX() - 1];
 
-          if((ag.getX() + 1) >= n)
+          if((ag.getX() + 1) >= tamanho)
             dir = NULO;
           else
             dir = matriz[ag.getY()][ag.getX() + 1];
@@ -95,7 +127,7 @@ public class Ambiente {
             cima = NULO;
           else cima = matriz[ag.getY() - 1][ag.getX()];
 
-          if((ag.getY() + 1) >= n)
+          if((ag.getY() + 1) >= tamanho)
             baixo = NULO;
           else
             baixo = matriz[ag.getY() + 1][ag.getX()];
--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
 
   public static void main(String[] args) {
-    Ambiente ambiente = new Ambiente(10);
+    Ambiente ambiente = new Ambiente(12, 3, 3);
     ambiente.inserirAgente(new Agente(0, 0, 'd', 0));
     ambiente.simular();
     //System.out.println("Hello World");
