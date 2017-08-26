f07265e66cb2fe62a8e0a09c4a40079ed57f2c7e
emanoelvianna <emanoel.vianna@outlook.com>
2017-08-26 00:12:36 -0300

    ambiente - parcial

--- src/Ambiente.java
+++ src/Ambiente.java
@@ -30,20 +30,18 @@ public class Ambiente {
 
   /* TODO Coloca elementos (lixeiras, lixo e carregadores) de forma randomica no ambiente  */
   private void carregar(int n_lixeiras, int n_carregadores, int n_lixo) {
-    for(int i = 0; i < tamanho; i++)
-      for(int j = 0; j < tamanho; j++)
+    for (int i = 0; i < tamanho; i++)
+      for (int j = 0; j < tamanho; j++)
         matriz[i][j] = LIMPO;
   }
 
   /* Coloca elementos no ambiente - estatico */
   private void carregar() {
-    for(int i = 0; i < tamanho; i++)
-      for(int j = 0; j < tamanho; j++)
+    for (int i = 0; i < tamanho; i++) {
+      for (int j = 0; j < tamanho; j++) {
         matriz[i][j] = LIMPO;
-
-    matriz[4][0] = RECARGA;
-    matriz[2][0] = LIXEIRA;
-    matriz[4][9] = RECARGA;
+      }
+    }
 
     inserirParedes();
     inserirLixeiras();
@@ -57,23 +55,29 @@ public class Ambiente {
 
   /* Insere paredes no ambiente */
   public void inserirParedes() {
-    // dividindo a matriz em 3 partes
-    int metade = this.tamanho/3;
-    
-    for(int i = 0; i < tamanho; i++) {
-      for(int j = 0; j < tamanho; j++) {
-        // desconsiderando as primeiras linhas da matriz
-        if(i < (this.tamanho-2) && i > 2) {
-          this.matriz[i][metade-2] = PAREDE;
-          this.matriz[i][metade+2] = PAREDE;
-        }
+    for (int i = 0; i < tamanho; i++) {
+      for (int j = 0; j < tamanho; j++) {
+        // realizando o tratamento lateral
+        this.matriz[i][this.tamanho - 3] = PAREDE;
+        //this.matriz[i][0 - 3] = PAREDE;
+        
+        //this.matriz[i][this.tamanho + 2] = PAREDE;
       }
     }
   }
 
   //TODO: adicionar lixeiras randomicas considerando as paredes
   public void inserirLixeiras() {
-
+    int auxiliar = this.quantidadeLixeiras;
+    for (int i = 0; i < tamanho; i++) {
+      for (int j = 0; j < tamanho; j++) {
+        if(this.matriz[i][j] != PAREDE || this.matriz[i][j] != SUJEIRA || this.matriz[i][j] != RECARGA) {
+          if(auxiliar > 0) {
+            //this.matriz[i][j] = LIXEIRA;
+          }
+        }
+      }
+    }
   }
 
   //TODO: adicionar sujeiras randomicas considerando as paredes
@@ -83,12 +87,12 @@ public class Ambiente {
 
   /* Imprime o estado atual do ambiente com seus agentes */
   public void print() {
-    for(int i = 0; i < tamanho; i++) {
-      for(int j = 0; j < tamanho; j++) {
+    for (int i = 0; i < tamanho; i++) {
+      for (int j = 0; j < tamanho; j++) {
 
         // Verifica se algum agente esta nesta Posicao
-        for(Agente ag : agentes) {
-          if(ag.getX() == j && ag.getY() == i)
+        for (Agente ag : agentes) {
+          if (ag.getX() == j && ag.getY() == i)
             System.out.print("A ");
           else
             System.out.print(matriz[i][j] + " ");
@@ -102,32 +106,33 @@ public class Ambiente {
   /* Executa a simulacao dos agentes no ambiente */
   public void simular() {
     System.out.print(ANSI_CLS + ANSI_HOME);
-    while(true) {
+    while (true) {
       print();
       try {
         Thread.sleep(500);
         System.out.print(ANSI_CLS + ANSI_HOME);
         System.out.flush();
 
-        for(Agente ag : agentes) {
+        for (Agente ag : agentes) {
           char atual, esq, dir, cima, baixo;
           atual = matriz[ag.getY()][ag.getX()];
 
-          if((ag.getX() - 1) < 0)
+          if ((ag.getX() - 1) < 0)
             esq = NULO;
           else
             esq = matriz[ag.getY()][ag.getX() - 1];
 
-          if((ag.getX() + 1) >= tamanho)
+          if ((ag.getX() + 1) >= tamanho)
             dir = NULO;
           else
             dir = matriz[ag.getY()][ag.getX() + 1];
 
-          if((ag.getY() - 1) < 0)
+          if ((ag.getY() - 1) < 0)
             cima = NULO;
-          else cima = matriz[ag.getY() - 1][ag.getX()];
+          else
+            cima = matriz[ag.getY() - 1][ag.getX()];
 
-          if((ag.getY() + 1) >= tamanho)
+          if ((ag.getY() + 1) >= tamanho)
             baixo = NULO;
           else
             baixo = matriz[ag.getY() + 1][ag.getX()];
@@ -135,7 +140,7 @@ public class Ambiente {
           ag.atualizar(atual, esq, dir, cima, baixo);
         }
 
-      } catch(Exception e) {
+      } catch (Exception e) {
         e.printStackTrace();
         System.exit(1);
       }
