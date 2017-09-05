56783fcd7688cc884f92135dc63a0d6d7d1149d0
Emanoel Vianna <emanoel.vianna@outlook.com>
2017-09-05 20:39:11 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 public class App {
 
 	public static void main(String[] args) {
-		Ambiente ambiente = new Ambiente(12, 3, 3);
+		//Ambiente ambiente = new Ambiente(Integer.parseInt(args), 3, 3);
+		Ambiente ambiente = new Ambiente(Integer.parseInt(args[0]), 3, 3);
 		ambiente.inserirAgente(new Agente(0, 0, 'd', 0));
 		ambiente.simular();
 		// System.out.println("Hello World");
 	}
-
 }
