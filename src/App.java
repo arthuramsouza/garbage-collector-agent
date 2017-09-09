fb3c9fcbb047b02ad277ce9c3ed1933e6b517d79
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2017-09-09 04:24:43 -0300

    VARREDURA COMPLETA FUNCIONANDO!!!

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
+
 public class App {
 
-	public static void main(String[] args) {
-		Ambiente ambiente = new Ambiente(Integer.parseInt(args[0]), 3, 3);
-		ambiente.inserirAgente(new Agente(0, 0, 'd', 0));
-		ambiente.simular();
-	}
+    public static void main(String[] args) {
+        Ambiente ambiente = new Ambiente(Integer.parseInt(args[0]), 3, 3);
+        ambiente.inserirAgente(new Agente(0, 0, 'd', 0, ambiente));
+        ambiente.simular();
+    }
 }
