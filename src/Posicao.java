26a32db8dfb838e1659dd9a9a0ac8b1a0c7ffea5
Gabriell Araujo <ZK9RX6@users.noreply.github.com>
2017-09-09 20:36:40 -0300

    CÓDIGO MAIS COMPLETO!!!

---
+++ src/Posicao.java
@@ -0,0 +1,27 @@
+
+public class Posicao {
+
+    private int x;
+    private int y;
+
+    public Posicao(int x, int y) {
+        this.x = y;
+        this.y = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
